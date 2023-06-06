5f88b455b74c69c113879636db8eb8955a30d50a
Blackbird410 <ntsrigaud14@gmail.com>
2023-06-06 12:00:57 +0800

    Project finished

--- code.js
+++ code.js
@@ -8,18 +8,44 @@ cont_row.className = 'cont-row';
 const grid = document.createElement('div');
 grid.className = 'grid';
 
-for (let i = 0; i < 16; i++) {
-    cont_row.appendChild(grid.cloneNode(true));
-}
+button = document.createElement('button');
+button.textContent = "Add grids";
+body.appendChild(button);
 
-for (let i = 0; i < 16; i++) {
-    container.appendChild(cont_row.cloneNode(true));
-}
-
-body.appendChild(container);
-
-container.addEventListener('mouseover', (event) => {
-    if (event.target.className === 'grid') {
-        event.target.style.backgroundColor = 'orange';
+function generateGrid(n, row, box) {
+    for (let i = 0; i < n; i++) {
+        row.appendChild(grid.cloneNode(true));
     }
+    
+    for (let i = 0; i < n; i++) {
+        box.appendChild(row.cloneNode(true));
+    }
+    body.appendChild(box);
+}
+
+let nSquares = 16;
+generateGrid(nSquares, cont_row, container);
+
+let b = 0;
+
+document.addEventListener('mouseover', (event) => {
+    if (event.target.className === 'grid') {
+        b = b <= 100 ? b + 10: b = 0; 
+        event.target.style.backgroundColor = `rgb(${b}%, ${b}%, ${b}%)`;
+    }
+});
+
+button.addEventListener('click', (event) => {
+    do {
+        nSquares = parseInt(prompt('How much squares do you want ? (Max : 100)'));
+    } while (nSquares < 0 || nSquares > 100);
+    
+    let box = document.querySelector('.container');
+    box.remove();
+    
+    let newCont = document.createElement('div');
+    let newRows = document.createElement('div');
+    newCont.className = 'container';
+    newRows.className = 'cont-row';
+    generateGrid(nSquares, newRows, newCont);
 });
